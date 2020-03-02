6e39946b84668ea88aa2101b20c40dff805b715f
Student101 <student101@PSAdmins-MacBook-Air-3.local>
2020-03-02 17:18:33 -0500

    Updates

---
+++ precache-manifest.c4ea6bd615de12c071007aba1f35601e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ab4c9ce2e26172c1abb73cada5165e8",
+    "url": "/react-mariah-spotify/index.html"
+  },
+  {
+    "revision": "2a3cbfd88518bdef7a23",
+    "url": "/react-mariah-spotify/static/css/main.addb4e42.chunk.css"
+  },
+  {
+    "revision": "9d462df637b1d20e48cc",
+    "url": "/react-mariah-spotify/static/js/2.85268578.chunk.js"
+  },
+  {
+    "revision": "af1b71dc8f7cc14f3b4c1f4697374759",
+    "url": "/react-mariah-spotify/static/js/2.85268578.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a3cbfd88518bdef7a23",
+    "url": "/react-mariah-spotify/static/js/main.d4077b5b.chunk.js"
+  },
+  {
+    "revision": "8d372f3f47e7342eb9d7",
+    "url": "/react-mariah-spotify/static/js/runtime-main.f3dab3de.js"
+  },
+  {
+    "revision": "8e0f80106be4cff67b443e893184fd68",
+    "url": "/react-mariah-spotify/static/media/logo.8e0f8010.svg"
+  }
+]);
